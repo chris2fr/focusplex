f4e3d607ab1dfeb9d4b5788ad6fb7c6eafa661d1
Chris Mann <christopher@mann.fr>
2018-12-09 21:54:59 +0100

    More satisfactory zoom

--- views.py
+++ views.py
@@ -9,22 +9,32 @@ def index(request):
     whats = ()
     what_form = ""
     zoom_id = request.GET.get('zoom')
+    
     if request.user.is_authenticated:
         whats = What.objects.order_by('result__id').filter(created_by=request.user)
-        
         what_form = WhatForm()
-        if request.method == 'POST':
-            # create a form instance and populate it with data from the request:
-            # what_form = WhatForm(request.POST)
-            # check whether it's valid:
-            #if what_form.is_valid():
-            # process the data in form.cleaned_data as required
-            action = request.POST.get("action")
-            what = What.objects.create(action=action,created_by=request.user)
-            if request.POST.get("result"):
-                result = What.objects.get(pk=request.POST.get("result"))
-                what.result = result
-            what.save()
+    else:
+        whats = What.objects.order_by('result__id').filter(public=True)
+    if(zoom_id):
+        whati = What.objects.get(pk=zoom_id)
+        if whati.result:
+            whats = whats.filter(Q(id=zoom_id) | Q(result__id=zoom_id) | Q(id=whati.result.id))
+        else: 
+            whats = whats.filter(Q(id=zoom_id) | Q(result__id=zoom_id) )
+
+       
+    if request.user.is_authenticated and request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        # what_form = WhatForm(request.POST)
+        # check whether it's valid:
+        #if what_form.is_valid():
+        # process the data in form.cleaned_data as required
+        action = request.POST.get("action")
+        what = What.objects.create(action=action,created_by=request.user)
+        if request.POST.get("result"):
+            result = What.objects.get(pk=request.POST.get("result"))
+            what.result = result
+        what.save()
         what_form.fields['result'].choices = (('','[Top]'),)
         for what in whats:
             what_form.fields['result'].choices.append((str(what.id),what.action))
@@ -33,15 +43,6 @@ def index(request):
         if request.method == 'POST':
             what_form.fields['result'].initial = request.POST.get("result")
             what_form.fields['result'].value = request.POST.get("result")
-            
-    else:
-        query = Q(public=True)
-        if(zoom_id):
-            query = query & ( Q(id=zoom_id) | Q(result__id=zoom_id) )
-        whats = What.objects.order_by('result__id').filter(query)
-        
-    if(zoom_id):
-        whats = whats.filter(Q(result__id=zoom_id) |  Q(id=zoom_id) )
     
     return render(request, 'why/index.html', {'whats':whats,'what_form':what_form})
 
