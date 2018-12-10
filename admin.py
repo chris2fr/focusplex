daa46f1d6a3fe14a2a801639271c2fff5e763451
Chris Mann <christopher@mann.fr>
2018-12-10 09:51:58 +0100

    Better clickflow staying in the same tab and sometimes on the same zoom.

--- admin.py
+++ admin.py
@@ -8,6 +8,7 @@ class WhatAdmin(admin.ModelAdmin):
     fields = ['action', 'result','with_who','public']
     list_display = ('action', 'result','with_who','public')
     
+    
     def save_model(self, request, instance, form, change):
         instance.owner = request.user
 
@@ -18,13 +19,23 @@ class WhatAdmin(admin.ModelAdmin):
         form.save_m2m()
         return instance
         
+    def redirect_zoom(self, zoom_id):
+        """A private method really"""
+        if(zoom_id>0):
+            id = str(zoom_id)
+            redir = "/?zoom=" + id
+        else:
+            redir = '/'
+        return redirect(redir)
+        
     def response_add(self, request, obj, post_url_continue=None):
         """Redirect to root on add"""
-        return redirect('/')
+        return self.redirect_zoom(obj.result.id)
+        
 
     def response_change(self, request, obj):
         """Redirect to root on change"""
-        return redirect('/')
+        return self.redirect_zoom(obj.result.id)
 
     def response_delete(self, request, obj, post_url_continue=None):
         """Redirect to root on delete"""
