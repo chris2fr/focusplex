a9fb19b1d21fc886bed9dfddd5fc46bb14ed64e0
Chris Mann <christopher@mann.fr>
2018-12-11 18:36:12 +0100

    cosmetic changes

--- views.py
+++ views.py
@@ -118,7 +118,7 @@ def read(request, id):
             
     if request.user.is_authenticated:
         what_new_form.fields['result'].widget = django.forms.widgets.HiddenInput()
-        what_now_form.fields['result'].choices = (('',"t'ask why"),)
+        what_now_form.fields['result'].choices = (('',"task why"),)
         whats = []
         for what in what_ups:
             # what_new_form.fields['result'].choices.append((str(what.id),what.action))
