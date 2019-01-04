c4381259ad20e20dab8a8c89844693d0f2a4f678
Chris Mann <chrisr@mann.fr>
2019-01-04 19:06:13 +0100

    Changed message

--- forms.py
+++ forms.py
@@ -9,7 +9,7 @@ class WhatForm(forms.Form):
     action = forms.CharField(label="initiating action",
         max_length=255,
         required=True,
-        help_text="verb noun ...",
+        help_text="verb noun",
         )
     # id = forms.HiddenField()
     
